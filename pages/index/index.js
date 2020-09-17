f08fa6c2748f52191e294ac54db04d80ee468f0c
jiaqihu-supreme <1091853035@qq.com>
2020-09-17 19:57:33 +0800

    完成首页楼层展示和分类页面展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,24 +6,45 @@ Page({
    */
   data: {
     //轮播图数组
-    swiperList:[]
+    swiperList:[],
+    cateList:[],
+    floorList:[],
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //发送异步请求获取轮播图数据
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+  //获取轮播图数据
+  getSwiperList(){
     request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
     .then(result=>{
       this.setData({
-            swiperList:result.data.message
+        swiperList:result.data.message
       })
-    })  
-    // success:(result) =>{
-      //   console.log(result);
-      //   this.setData({
-      //     swiperList:result.data.message
-      //   })
-      // }
+    }) 
   },
+  //获取分类导航数据
+  getCateList(){
+    request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+    .then(result=>{
+      console.log(result);
+      this.setData({
+        cateList:result.data.message
+      })
+    }) 
+  },
+  //获取楼层数据
+  getFloorList(){
+    request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'})
+    .then(result=>{
+      console.log(result);
+      this.setData({
+        floorList:result.data.message
+      })
+    }) 
+  }
 })
